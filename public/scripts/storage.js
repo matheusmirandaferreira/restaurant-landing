5132f1b46a9d9f3a888857d1bc35e43fea0db678
Matheus de Miranda Ferreira <ohhhmiranha@gmail.com>
2020-11-10 10:46:35 -0300

    Código comentado

--- public/scripts/storage.js
+++ public/scripts/storage.js
@@ -1,11 +1,18 @@
 function saveUserName() {
+  // pegar nome do usuario
   const name = document.getElementById("name").value;
+  // salvar no localStorage
   localStorage.setItem("userName", name);
 }
 
+// encontrar a tag <strong>
 if (document.querySelector("#welcome strong")) {
+  // pegar a tag <strong>
   const div = document.querySelector("#welcome strong");
+  // pegar o nome pelo localStorage
   const userName = localStorage.getItem("userName");
+  // pegar o primeiro nome do usuário
   const firstName = userName.split(" ")[0]
+  // inserir dado no HTML
   div.append(firstName);
 }
